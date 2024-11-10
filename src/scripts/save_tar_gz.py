9f8ce7eba2e53e7f5edb8cb103a087710f997e97
Felix <felix.schmeding@epfl.ch>
2024-11-10 10:58:11 +0100

    deplacer load_data - ajout d'ne fonction qui save les csv

---
+++ src/scripts/save_tar_gz.py
@@ -0,0 +1,22 @@
+import os
+from src.scripts.load_data import load_data
+
+def tar_gz_to_csv(load_path, save_path, load_text=False):
+    data_sets = load_data(load_path, bool_load_txt=load_text)
+    ratebeer_path = 'RateBeer'
+    beeradvocate_path = 'BeerAdvocate'
+    matched_path = 'MatchedBeerData'
+
+    for data in data_sets.keys():
+        path = save_path
+        if "BeerAdvocate" in data:
+           path = os.path.join(save_path, beeradvocate_path)
+        elif "Rate" in data:
+            path = os.path.join(save_path, ratebeer_path)
+        elif "matched" in data:
+            path = os.path.join(save_path, matched_path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        data_sets[data].to_csv(os.path.join(path, data))
+
+    return data_sets
